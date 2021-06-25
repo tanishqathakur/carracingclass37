0610135ef47dcb31bb30a98b6707f961310ff5c5
tanishqathakur <75884006+tanishqathakur@users.noreply.github.com>
2021-06-25 13:53:55 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,32 @@
+class Player {
+  constructor(){
+    this.index=null;
+this.distance=0;
+this.name=null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(name){
+    var playerIndex = "player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+  static getplayerinfo(){
+    var playerinforef = database.ref('player')
+     
+  }
+}
